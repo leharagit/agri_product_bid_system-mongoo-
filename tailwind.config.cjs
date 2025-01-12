1e94280fd8fd682b3ae4d65274581933fed60bcc
abdulmalik28 <malikmn-ps20275@stu.kln.ac.lk>
2025-01-12 10:01:45 +0530

    Add files via upload

---
+++ tailwind.config.cjs
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "gray-20": "#7f8c8d",
+        "gray-50": "#34495e",
+        "gray-100": "#2c3e50",
+        "gray-500": "#1a242f",
+        "primary-100": "#85c1e9",
+        "primary-300": "#3498db",
+        "primary-500": "#1f6f9f",
+        "secondary-400": "#e5f75c",
+        "secondary-500": "#ddf52c",
+      },
+      backgroundImage: (theme) => ({
+        "gradient-yellowred":
+          "linear-gradient(90deg, #FF616A 0%, #FFC837 100%)",
+        "mobile-home": "url('./assets/HomePageGraphic.png')",
+      }),
+      fontFamily: {
+        dmsans: ["DM Sans", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      content: {
+        evolvetext: "url('./assets/EvolveText.png')",
+        abstractwaves: "url('./assets/AbstractWaves.png')",
+        sparkles: "url('./assets/Sparkles.png')",
+        circles: "url('./assets/Circles.png')",
+      },
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
+    },
+  },
+  plugins: [],
+};
